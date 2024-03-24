95797cafa0b2139f8e2ad7363819519d506f75e9
nstarke <starke.nicholas@gmail.com>
2024-03-24 12:43:31 -0500

    adding lst file with opcode/disassembly

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nasm -f bin -o bootfuzz.img bootfuzz.asm
+nasm -f bin -o bootfuzz.img -l bootfuzz.lst bootfuzz.asm
 xxd bootfuzz.img > hexdump.txt
 qemu-system-i386 -fda bootfuzz.img -nographic -accel kvm
 # CTRL-A + X will exit out of the qemu console
